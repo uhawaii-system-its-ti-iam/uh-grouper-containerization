bd2cd4f8f733fc235139819d0bdcbaea648a00e9
mhodges <mhodges@hawaii.edu>
2023-12-13 12:09:08 -1000

    Build 120, generate self signed cert, not yet used.
    stack.yml runs the script before starting up the service.
    S
    Script generates cert for Grouper to pick up.
    Removed unused Grouper settings from the docker file, per build 119 testing.
    Logrotate image also needs ncompress.

--- src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
+++ src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# opt/keypairs/self-signed-cert.sh - generate the image's self-signed cert
+# opt/tomcat/bin/self-signed-cert.sh - generate the image's self-signed cert
 #
 # For the chmod that makes this file executable see the Dockerfile for each of
 # the Grouper services.
@@ -7,15 +7,18 @@
 # The host domain is defined in the Portainer stack.
 SUBJECT="/C=US/ST=Hawaii/O=University of Hawaii/CN=${CONTAINER_HOST_DOMAIN}"
 
+# Per Grouper specification.
+CERT_DIR="/opt/grouper/certs/client"
+
+openssl req -x509 -newkey rsa:4096 -keyout $CERT_DIR/key.pem -out $CERT_DIR/cert.pem -sha256 -days 7332 -nodes -subj "${SUBJECT}"
+
 # Referenced in the tomcat/server.xml file.
 #FILE="/opt/keypairs/keystore"
-FILE="/opt/grouper/certs/anchors/keystore"
 
 # In the future we anticipate using the PEM.
 #openssl req -x509 -sha256 -nodes -days 7332 -newkey rsa:4096 -keyout $FILE.key -out $FILE.crt -subj "${SUBJECT}"
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out $FILE.pem -sha256 -days 7332 -nodes -subj "${SUBJECT}"
 
 # For now we will continue using the P12.
 #openssl pkcs12 -export -out $FILE.p12 -inkey $FILE.key -in $FILE.crt -passout pass:
 
-echo "exiting self-signed cert script with status: ${?}."
+echo "Exit self-signed cert script with status: ${?}."
