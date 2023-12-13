51683a0ffcac6c41b454366f61b91b1f25adb44f
mhodges <mhodges@hawaii.edu>
2023-12-13 09:56:35 -1000

    Build 117, self-signed cert #1
    Deploy script to generate the cert, but don't switch to it yet.  The goal is to put in the path of the default image build rather than using a customer server.xml file.

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ FROM docker.io/i2incommon/grouper:${GROUPER_VERSION} as install
 
 COPY /src/main/docker/slashRoot/ /
 
-RUN chmod +x /opt/keypairs/self-signed-cert.sh
+#RUN chmod +x /opt/tomcat/bin/self-signed-cert.sh
--- src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
+++ src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
@@ -8,12 +8,14 @@
 SUBJECT="/C=US/ST=Hawaii/O=University of Hawaii/CN=${CONTAINER_HOST_DOMAIN}"
 
 # Referenced in the tomcat/server.xml file.
-FILE="/opt/keypairs/keystore"
+#FILE="/opt/keypairs/keystore"
+FILE="/opt/grouper/certs/anchors/keystore"
 
 # In the future we anticipate using the PEM.
-openssl req -x509 -sha256 -nodes -days 7332 -newkey rsa:4096 -keyout $FILE.key -out $FILE.crt -subj "${SUBJECT}"
+#openssl req -x509 -sha256 -nodes -days 7332 -newkey rsa:4096 -keyout $FILE.key -out $FILE.crt -subj "${SUBJECT}"
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out $FILE.pem -sha256 -days 7332 -nodes -subj "${SUBJECT}"
 
 # For now we will continue using the P12.
-openssl pkcs12 -export -out $FILE.p12 -inkey $FILE.key -in $FILE.crt -passout pass:
+#openssl pkcs12 -export -out $FILE.p12 -inkey $FILE.key -in $FILE.crt -passout pass:
 
 echo "exiting self-signed cert script with status: ${?}."
