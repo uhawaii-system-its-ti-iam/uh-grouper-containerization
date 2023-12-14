5aa6269dcaeef942870a331687c626390643ee1f
mhodges <mhodges@hawaii.edu>
2023-12-14 10:19:59 -1000

    Build 136, resort to a custom server.xml after all
    Update PEM script to save key and cert to the expected locations, matched to the server.xml settings.

--- src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
+++ src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # opt/tomcat/bin/self-signed-cert.sh - generate the image's self-signed cert
 #
+# This cert will only be used by the F5.
+#
 # For the chmod that makes this file executable see the Dockerfile for each of
 # the Grouper services.
 
@@ -8,12 +10,10 @@
 #SUBJECT="/C=US/ST=Hawaii/O=University of Hawaii/CN=${DEPLOYMENT_CN}"
 SUBJECT="/C=US/ST=Hawaii/O=University of Hawaii/CN=idp-future-dev.its.hawaii.edu"
 
-# Per Grouper specification.
-CERT_DIR="/opt/grouper/certs/client"
+echo "RUN opt/tomcat/bin/self-signed-cert.sh"
 
-echo "Begin self-signed cert script. CN=${DEPLOYMENT_CN}"
+sudo openssl req -new -x509 -sha256 -newkey rsa:4096 -nodes -keyout /etc/pki/tls/private/cert.key \
+                 -days 7332 -out /etc/pki/tls/certs/cert.pem \
+                 -subj "${SUBJECT}"
 
-openssl req -x509 -newkey rsa:4096 -keyout $CERT_DIR/cert.key -out $CERT_DIR/cert.pem -sha256 -days 7332 -nodes -subj "${SUBJECT}"
-openssl x509 -in $CERT_DIR/cert.pem -text -noout
-
-echo "Exit self-signed cert script."
+echo "Exit opt/tomcat/bin/self-signed-cert.sh - status: ${?}"
