17fba9625f4f111f3f4cfd7139ee6fe56909387d
mhodges <mhodges@hawaii.edu>
2023-12-13 15:03:13 -1000

    Build 125, add diagnostics to the cert script
    Determine why the subject is reported as empty.
    Test more cleanup of the Dockerfiles.

--- src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
+++ src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
@@ -10,15 +10,9 @@ SUBJECT="/C=US/ST=Hawaii/O=University of Hawaii/CN=${CONTAINER_HOST_DOMAIN}"
 # Per Grouper specification.
 CERT_DIR="/opt/grouper/certs/client"
 
+echo "Begin self-signed cert script."
+
 openssl req -x509 -newkey rsa:4096 -keyout $CERT_DIR/cert.key -out $CERT_DIR/cert.pem -sha256 -days 7332 -nodes -subj "${SUBJECT}"
+openssl x509 -in cert.pem -text -noout
 
-# Referenced in the tomcat/server.xml file.
-#FILE="/opt/keypairs/keystore"
-
-# In the future we anticipate using the PEM.
-#openssl req -x509 -sha256 -nodes -days 7332 -newkey rsa:4096 -keyout $FILE.key -out $FILE.crt -subj "${SUBJECT}"
-
-# For now we will continue using the P12.
-#openssl pkcs12 -export -out $FILE.p12 -inkey $FILE.key -in $FILE.crt -passout pass:
-
-echo "Exit self-signed cert script with status: ${?}."
+echo "Exit self-signed cert script."
