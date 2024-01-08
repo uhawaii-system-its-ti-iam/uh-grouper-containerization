c75fbc44c46076f7d931051b4e0209b41cb455e3
mhodges <mhodges@hawaii.edu>
2024-01-08 11:37:18 -1000

    Build 163, Fix CONTAINER_CERT_SUBJECT
    The surrounding quotes in Portainer are not needed.

--- src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
+++ src/main/docker/slashRoot/opt/tomcat/bin/self-signed-cert.sh
@@ -6,9 +6,8 @@
 # For the chmod that makes this file executable see the Dockerfile for each of
 # the Grouper services.
 echo "Enter opt/tomcat/bin/self-signed-cert.sh"
-echo "RUN opt/tomcat/bin/self-signed-cert.sh"
+echo "CONTAINER_CERT_SUBJECT: ${CONTAINER_CERT_SUBJECT}"
 
-#sudo removed for testing purposes
 openssl req -new -x509 -sha256 -newkey rsa:4096 -nodes -keyout /etc/pki/tls/private/cert.key \
                  -days 7332 -out /etc/pki/tls/certs/cert.pem \
                  -subj "${CONTAINER_CERT_SUBJECT}"
